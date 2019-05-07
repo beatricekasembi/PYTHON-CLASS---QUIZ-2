024bfb1dcd3178fdc7d2577496e6ed03469227d9
beatricekasembi <kasembibeatrice@gmail.com>
2019-05-07 19:19:26 +0300

    QUIZ 2

---
+++ quiz.py
@@ -0,0 +1,55 @@
+def tens(x):
+	for x in range(1,10):
+		x=x*10
+		print(x)
+
+
+def list(b):
+	b = []
+	for x in b:
+		y=x*x
+		print(y)
+
+def sorted(s):
+	a = []
+	b = []
+	c = a+b
+	c.sort()
+
+def range_sum(r):
+	r = []
+	for x in range(n):
+		r.append(x)
+		sum(r)
+
+def largest(l):
+	l = []
+	l.sort()
+	l[n]
+
+def smallest(s):
+	s = []
+	s.sort()
+	s[0]
+
+def my_modulus(m):
+	u = range(10,20)
+	m = dict()
+	for x in u:
+		m[u]=u%3
+		print(m)
+
+def list(list):
+	q = balance
+	student1 = {"name":" ","balance":q}
+	student2 = {"name":" ","balance":q}
+	student3 = {"name":" ","balance":q}
+	students = []
+	for student in students:
+		list = "Hello {}, you balance is {}".format(student["name"],student["balance"])
+		print(list)
+
+
+
+
+
